31ab8948841aa0d118a667ad503f98455918bd8b
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-01-30 20:42:17 +0100

    Import libraries.Global variables set.

--- ping_pong.py
+++ ping_pong.py
@@ -1,3 +1,16 @@
 #Classic arcade game for two players.
 
-import pygame
+import pygame, random, math, time
+
+pygame.init()
+
+#game colors
+red = (255, 0, 0)
+green = (0, 255, 0)
+orange = (218, 176, 39)
+white = (255, 255, 255)
+black = (0, 0. 0)
+
+#screen dimensions
+screen_width = 600
+screen_lengh = 400
