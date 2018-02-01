81d8202ead08d0d26143a6bd3ad41a082953f19c
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-02-01 20:44:55 +0100

    Function game_screen() added.

--- ping_pong.py
+++ ping_pong.py
@@ -11,6 +11,11 @@ orange = (218, 176, 39)
 white = (255, 255, 255)
 black = (0, 0. 0)
 
-#screen dimensions
-screen_width = 600
-screen_lengh = 400
+def game_screen():
+    screen_width = 600
+    screen_lengh = 400
+    screen_area = pygame.display.set_mode((screen_width, screen_lengh))
+    pygame.display.set_caption('Enjoy Ping-pong!')
+    pygame.font.SysFont("arial", 30, bold=True)
+    
+
