446effad6bf527358e3009941be443f7871a88e2
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-02-07 19:58:51 +0100

    Variables with ball parameters added

--- ping_pong.py
+++ ping_pong.py
@@ -11,11 +11,22 @@ orange = (218, 176, 39)
 white = (255, 255, 255)
 black = (0, 0. 0)
 
-def game_screen():
+def main():
     screen_width = 600
     screen_lengh = 400
     screen_area = pygame.display.set_mode((screen_width, screen_lengh))
     pygame.display.set_caption('Enjoy Ping-pong!')
     pygame.font.SysFont("arial", 30, bold=True)
-    
+
+#define ball starting position
+    ball_x_axis = int(screen_width / 2)
+    ball_y_axis = int(screen_lengh / 2)
+
+#define how far ball will move on (distance in pixels)
+    ball_x_move = 3
+    ball_y_move = 3
+    ball_size = 25
+
+
+
 
