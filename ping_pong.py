9ff33b81c5762c2c6143f58f4485a276ea6830f2
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-02-09 23:07:45 +0100

    Paddles movement handling

--- ping_pong.py
+++ ping_pong.py
@@ -11,45 +11,52 @@ orange = (218, 176, 39)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-def game_ui():
-    screen_width = 600
-    screen_lengh = 400
-    screen_area = pygame.display.set_mode((screen_width, screen_lengh))
-    pygame.display.set_caption('Enjoy Ping-pong!')
-    pygame.font.SysFont("arial", 30, bold=True)
+screen_width = 600
+screen_lengh = 400
+screen_area = pygame.display.set_mode((screen_width, screen_lengh))
+pygame.display.set_caption('Enjoy Ping-pong!')
+pygame.font.SysFont("arial", 30, bold=True)
 
 #define ball starting position
-    ball_x_axis = int(screen_width / 2)
-    ball_y_axis = int(screen_lengh / 2)
+ball_x_axis = int(screen_width / 2)
+ball_y_axis = int(screen_lengh / 2)
 
 #define how far ball will move on (distance in pixels)
-    ball_x_move = 3
-    ball_y_move = 3
-    ball_size = 25
+ball_x_move = 3
+ball_y_move = 3
+ball_size = 25
 
-    paddleA_xaxis_position = 10
-    paddleA_yaxis_position = 10
-    paddleA_width = 25
-    paddleA_heigh = 100
+paddleA_xaxis_position = 10
+paddleA_yaxis_position = 10
+paddleA_width = 25
+paddleA_heigh = 100
 
-    paddleB_xaxis_position = (screen_width - 35)
-    paddleB_yaxis_position = 10
-    paddleB_width = 25
-    paddleB_heigh = 100
+paddleB_xaxis_position = (screen_width - 35)
+paddleB_yaxis_position = 10
+paddleB_width = 25
+paddleB_heigh = 100
 
-    first_player_score = 0
-    second_player_score = 0
+first_player_score = 0
+second_player_score = 0
 
-def main():
-    program_run = True
-    game_ui()
-    pygame.mouse.set_visible(False)
-    while program_run is True:
-        pressed_button = pygame.key.get_pressed()
-        pygame.key.set_repeat()
-        for event in pygame.event.get():
-            if pygame.type == pygame.QUIT:
-                program_run = False
+program_run = True
+pygame.mouse.set_visible(False)
+while program_run is True:
+    pressed_button = pygame.key.get_pressed()
+    pygame.key.set_repeat()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            program_run = False
+    if pressed_button [pygame.K_ESCAPE]:
+        program_run = False
+    if pressed_button [pygame.K_w]:
+        paddleA_yaxis_position -= 5
+    if pressed_button [pygame.K_s]:
+        paddleA_yaxis_position += 5
+    if pressed_button [pygame.K_UP]:
+        paddleB_yaxis_position -= 5
+    if pressed_button [pygame.K_DOWN]:
+        paddleB_yaxis_position += 5
 
 
 
