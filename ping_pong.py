95fa29930ed6498a4883bcd702d78dfc3fcae85d
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-02-11 21:14:18 +0100

    While loop fixed. Game screenshot added.

--- ping_pong.py
+++ ping_pong.py
@@ -15,7 +15,7 @@ screen_width = 600
 screen_lengh = 400
 screen_area = pygame.display.set_mode((screen_width, screen_lengh))
 pygame.display.set_caption('Enjoy Ping-pong!')
-pygame.font.SysFont("arial", 30, bold=True)
+font = pygame.font.SysFont("arial", 30, bold=True)
 
 #define ball starting position
 ball_x_axis = int(screen_width / 2)
@@ -58,51 +58,55 @@ while program_run is True:
     if pressed_button [pygame.K_DOWN]:
         paddleB_yaxis_position += 5
 
-ball_x_axis += ball_x_move
-ball_y_axis += ball_y_move
+    ball_x_axis += ball_x_move
+    ball_y_axis += ball_y_move
 # Cushion colision detection
-if ball_y_axis - ball_size <= 0 or ball_y_axis + ball_size >= screen_lengh:
-    ball_y_move *= -1
+    if ball_y_axis - ball_size <= 0 or ball_y_axis + ball_size >= screen_lengh:
+        ball_y_move *= -1
 
-if paddleA_yaxis_position < 0:
-    paddleA_yaxis_position = 0
-elif paddleA_yaxis_position + paddleA_heigh > screen_lengh:
-    paddleA_yaxis_position = screen_lengh - paddleA_heigh
+    if paddleA_yaxis_position < 0:
+        paddleA_yaxis_position = 0
+    elif paddleA_yaxis_position + paddleA_heigh > screen_lengh:
+        paddleA_yaxis_position = screen_lengh - paddleA_heigh
 
-if paddleB_yaxis_position < 0:
-    paddleB_yaxis_position = 0
-elif paddleB_yaxis_position + paddleB_heigh > screen_lengh:
-    paddleB_yaxis_position = screen_lengh - paddleB_heigh
+    if paddleB_yaxis_position < 0:
+        paddleB_yaxis_position = 0
+    elif paddleB_yaxis_position + paddleB_heigh > screen_lengh:
+        paddleB_yaxis_position = screen_lengh - paddleB_heigh
 
 #left paddle ball collision
-if ball_x_axis < paddleA_xaxis_position + paddleA_width and ball_y_axis >= paddleA_yaxis_position and ball_y_axis <= paddleA_yaxis_position + paddleA_heigh:
-    ball_x_move *= -1
+    if ball_x_axis < paddleA_xaxis_position + paddleA_width and ball_y_axis >= paddleA_yaxis_position and ball_y_axis <= paddleA_yaxis_position + paddleA_heigh:
+        ball_x_move *= -1
 
 #right paddle ball collison
-if ball_x_axis > paddleB_xaxis_position and ball_y_axis >= paddleB_yaxis_position and ball_y_axis <= paddleB_yaxis_position + paddleB_heigh:
-    ball_x_move *= -1
+    if ball_x_axis > paddleB_xaxis_position and ball_y_axis >= paddleB_yaxis_position and ball_y_axis <= paddleB_yaxis_position + paddleB_heigh:
+        ball_x_move *= -1
 
-#determine when player2 scores
-if ball_x_axis <= 0:
-    second_player_score += 1
-    ball_x_axis = int(screen_width /2)
-    ball_y_axis = int(screen_lengh /2)
-elif ball_y_axis >= screen_width:
-    first_player_score +=1
-    ball_x_axis = int(screen_width /2)
-    ball_y_axis = int(screen_lengh /2)
+#determine when players score
+    if ball_x_axis <= 0:
+        second_player_score += 1
+        ball_x_axis = int(screen_width /2)
+        ball_y_axis = int(screen_lengh /2)
+    elif ball_x_axis >= screen_width:
+        first_player_score +=1
+        ball_x_axis = int(screen_width /2)
+        ball_y_axis = int(screen_lengh /2)
 
 #graphical content
-screen_area.fill(green)
-paddleA = pygame.draw.rect(screen_area,red,(paddleA_xaxis_position, paddleA_yaxis_position, paddleA_heigh, paddleA_width), 0)
-paddleB = pygame.draw.rect(screen_area,red,(paddleB_xaxis_position,paddleB_yaxis_position,paddleB_heigh,paddleB_width),0)
-net = pygame.draw.line(screen_area,black,(300,5),(300,400))
-ball = pygame.draw.circle(screen_area,orange,(ball_x_axis,ball_y_axis),ball_size,0)
+    screen_area.fill(green)
+    paddleA = pygame.draw.rect(screen_area,red,(paddleA_xaxis_position, paddleA_yaxis_position, paddleA_width, paddleA_heigh), 0)
+    paddleB = pygame.draw.rect(screen_area,red,(paddleB_xaxis_position,paddleB_yaxis_position,paddleB_width, paddleB_heigh),0)
+    net = pygame.draw.line(screen_area,black,(300,5),(300,400))
+    ball = pygame.draw.circle(screen_area,orange,(ball_x_axis,ball_y_axis),ball_size,0)
 
 #score display
-score = font.render(str(first_player_score) + " " + str(second_player_score), 1, white)
-screen_area.blit(score, (screen_width /2 - score.get_width() /2, 10))
+    score = font.render(str(first_player_score) + " " + str(second_player_score), 1, white)
+    screen_area.blit(score, (screen_width /2 - score.get_width() /2, 10))
 
+    pygame.display.update()
+    time.sleep(0.016666667)
+
+pygame.quit()
 
 
 
